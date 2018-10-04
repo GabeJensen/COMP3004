62a83f0ff60b006ff4f1a998f078b7cf021ce568
Alex Gao <alexgao1@users.noreply.github.com>
2018-10-04 16:46:04 -0400

    TESTS: Added preliminary tests for Hand class; subject to change

--- src/test/java/core/HandTesting.java
+++ src/test/java/core/HandTesting.java
@@ -1,5 +1,81 @@
 package core;
 
-public class HandTesting {
+import junit.framework.TestCase;
 
+public class HandTesting extends TestCase{
+	public void testAddTiles() {
+		Hand h = new Hand();
+		Tile t1 = new Tile("R", "6");
+		Tile t2 = new Tile("O","6");
+		Tile t3 = new Tile("B", "6");
+		
+		h.addTile(t1);
+		h.addTile(t2);
+		h.addTile(t3);
+		// assert the tile count of the hand is no longer 0?
+		// assertEquals()
+	}
+	
+	public void testRemoveTiles() {
+		Hand h = new Hand();
+		Tile t1 = new Tile("R", "6");
+		Tile t2 = new Tile("O","6");
+		Tile t3 = new Tile("B", "6");
+		
+		h.addTile(t1);
+		h.addTile(t2);
+		h.addTile(t3);
+		// assertEquals()
+		
+		for (int i = 0; i < h.getTileCount(); i++) {
+			h.removeTile();
+		}
+		// assert that the tile hand count is now 0 
+		// assertEquals 
+	}
+	
+	public void testSortTiles() {
+		Hand h = new Hand();
+		Tile t1 = new Tile("R", "8");
+		Tile t2 = new Tile("O","4");
+		Tile t3 = new Tile("B", "2");
+		Tile t4 = new Tile("G", "11");
+				
+		h.addTile(t1);
+		h.addTile(t2);
+		h.addTile(t3);
+		h.addTile(t4);
+		
+		h.sortTiles();
+		
+		// how to assert the sort? 
+		
+		h.addTile(new Tile("R", "6"));
+		
+		h.sortTiles();
+		
+		// assert after adding a new tile
+	}
+	
+	public void testGetHandMelds() {
+		Hand h = new Hand();
+		Tile t1 = new Tile("R", "6");
+		Tile t2 = new Tile("O","7");
+		Tile t3 = new Tile("B", "8");
+		Tile t4 = new Tile("G", "9");
+		Tile t5 = new Tile("O", "12");
+		Tile t6 = new Tile("B", "2");
+				
+		h.addTile(t1);
+		h.addTile(t2);
+		h.addTile(t3);
+		h.addTile(t4);
+		h.addTile(t5);
+		h.addTile(t6);
+		
+		h.sortTiles();
+		
+		// assertEquals what will be the return of this h.getHandMelds call? string? object?
+		// will be returning R6,O7,B8,G9 in this case
+	}
 }
