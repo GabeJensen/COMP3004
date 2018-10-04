7ae893c611681aec75293f7963e769c60ccebfb0
Alex Gao <alexgao1@users.noreply.github.com>
2018-10-04 16:35:25 -0400

    TESTS: Preliminary tests for Tile class added, subject to change later on

--- src/test/java/core/TileTesting.java
+++ src/test/java/core/TileTesting.java
@@ -1,5 +1,28 @@
 package core;
 
-public class TileTesting {
+import junit.framework.TestCase;
 
+public class TileTesting extends TestCase{
+	public void testGetValue() {
+		Tile t = new Tile("R", "13");
+		Tile t1 = new Tile("B", "1");
+		Tile t2 = new Tile("G", "7");
+		Tile t3 = new Tile("O", "9");
+		assertEquals(13, t.getValue());
+		assertEquals(1, t1.getValue());
+		assertEquals(7, t2.getValue());
+		assertEquals(9, t3.getValue());
+	}
+	
+	public void testGetColor() {
+		Tile t = new Tile("R", "13");
+		Tile t1 = new Tile("B", "1");
+		Tile t2 = new Tile("G", "7");
+		Tile t3 = new Tile("O", "9");
+		assertEquals("Red", t.getColor());
+		assertEquals("Blue", t1.getColor());
+		assertEquals("Green", t2.getColor());
+		assertEquals("Orange", t3.getColor());
+	}
 }
+
