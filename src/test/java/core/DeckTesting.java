3d741b84ccc86af195817a1127031f781ece7392
Alex Gao <alexgao1@users.noreply.github.com>
2018-10-04 16:51:23 -0400

    TESTS: Preliminary tests for Deck class

--- src/test/java/core/DeckTesting.java
+++ src/test/java/core/DeckTesting.java
@@ -1,5 +1,20 @@
 package core;
 
-public class DeckTesting {
+import junit.framework.TestCase;
 
+public class DeckTesting extends TestCase{
+	public void testDealTile() {
+		Deck d = new Deck();
+		Tile t;
+		// or assert that it should be the first card since it isn't shuffled.
+		assertEquals(t, d.dealTile());
+	}
+	
+	public void testShuffleDeck() {
+		Deck d = new Deck();
+		Deck noshuffle = new Deck();
+		d.shuffleDeck();
+		
+		assertNotEquals(noshuffle, d);
+	}
 }
