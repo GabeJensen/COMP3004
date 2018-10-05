1314bc7f6df06b0837dafd975b598725a34dc4ba
Alex Gao <alexgao1@users.noreply.github.com>
2018-10-05 10:43:25 -0400

    TESTS: Modified the test cases a bit.

--- src/test/java/core/DeckTesting.java
+++ src/test/java/core/DeckTesting.java
@@ -1,20 +1,22 @@
 package core;
 
+import static org.junit.Assert.*;
+
 import junit.framework.TestCase;
 
 public class DeckTesting extends TestCase{
 	public void testDealTile() {
 		Deck d = new Deck();
-		Tile t;
-		// or assert that it should be the first card since it isn't shuffled.
-		assertEquals(t, d.dealTile());
+		// The last card to be added from the for loops is O, 13.
+		// The deck deals the cards from the last added, so we should expect O, 13 to be the card here since we don't shuffle upon creation.
+		assertArrayEquals(new String[] {"O", "13"}, d.dealTile().getInfo());
 	}
 	
 	public void testShuffleDeck() {
 		Deck d = new Deck();
 		Deck noshuffle = new Deck();
 		d.shuffleDeck();
-		
-		assertNotEquals(noshuffle, d);
+		// The following assert should rarely ever fail ... due to the nature of testing a randomize shuffle, maybe once in a while the first tile dealt could be the same.
+		assertNotEquals(noshuffle.dealTile().getInfo(), d.dealTile().getInfo());
 	}
 }
