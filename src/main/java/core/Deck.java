4b598650a511eeb81d824392d509de6daac5d47d
Alex Gao <alexgao1@users.noreply.github.com>
2018-10-05 11:02:45 -0400

    CODE: Deck class code

---
+++ src/main/java/core/Deck.java
@@ -0,0 +1,35 @@
+package core;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Deck {
+	private ArrayList<Tile> deck = new ArrayList<Tile>();
+	
+	public Deck() {
+		String[] colors = {"R", "B", "G", "O"};
+		String[] values = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"};
+		
+		int vLength = values.length;
+		
+		for (int d = 0; d < 2; d++) {
+			for (int c = 0; c < colors.length; c++) {
+				for (int v = 0; v < vLength; v++) {
+					deck.add(new Tile(colors[c], values[v]));
+				}
+			}
+		}
+	}
+	
+	public void shuffleDeck() {
+		Collections.shuffle(deck);
+	}
+	
+	public Tile dealTile() {
+		return deck.remove(deck.size() - 1);
+	}
+	
+	public int getTileCount() {
+		return deck.size();
+	}
+}
