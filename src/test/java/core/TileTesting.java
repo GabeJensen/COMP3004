2dc8f09ec8ed7071768cb842dfdd45289cff51cc
Alex Gao <alexgao1@users.noreply.github.com>
2018-10-05 12:03:47 -0400

    TEST: minor changes Tile tests

--- src/test/java/core/TileTesting.java
+++ src/test/java/core/TileTesting.java
@@ -1,6 +1,7 @@
 package core;
 
 import junit.framework.TestCase;
+import static org.junit.Assert.*;
 
 public class TileTesting extends TestCase{
 	public void testGetInfo() {
@@ -8,10 +9,10 @@ public class TileTesting extends TestCase{
 		Tile t1 = new Tile("B", "1");
 		Tile t2 = new Tile("G", "7");
 		Tile t3 = new Tile("O", "9");
-		assertEquals(new String[] {"R", "13"}, t.getInfo());
-		assertEquals(new String[] {"B", "1"}, t1.getInfo());
-		assertEquals(new String[] {"G", "7"}, t2.getInfo());
-		assertEquals(new String[] {"O", "9"}, t3.getInfo());
+		assertArrayEquals(new String[] {"R", "13"}, t.getInfo());
+		assertArrayEquals(new String[] {"B", "1"}, t1.getInfo());
+		assertArrayEquals(new String[] {"G", "7"}, t2.getInfo());
+		assertArrayEquals(new String[] {"O", "9"}, t3.getInfo());
 	}
 }
 
