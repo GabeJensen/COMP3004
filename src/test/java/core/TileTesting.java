676743b7ac0e34f066dd7afeead97a4135716781
Alex Gao <alexgao1@users.noreply.github.com>
2018-10-05 09:51:30 -0400

    TESTS: revised tile tests

--- src/test/java/core/TileTesting.java
+++ src/test/java/core/TileTesting.java
@@ -3,26 +3,15 @@ package core;
 import junit.framework.TestCase;
 
 public class TileTesting extends TestCase{
-	public void testGetValue() {
+	public void testGetInfo() {
 		Tile t = new Tile("R", "13");
 		Tile t1 = new Tile("B", "1");
 		Tile t2 = new Tile("G", "7");
 		Tile t3 = new Tile("O", "9");
-		assertEquals(13, t.getValue());
-		assertEquals(1, t1.getValue());
-		assertEquals(7, t2.getValue());
-		assertEquals(9, t3.getValue());
-	}
-	
-	public void testGetColor() {
-		Tile t = new Tile("R", "13");
-		Tile t1 = new Tile("B", "1");
-		Tile t2 = new Tile("G", "7");
-		Tile t3 = new Tile("O", "9");
-		assertEquals("Red", t.getColor());
-		assertEquals("Blue", t1.getColor());
-		assertEquals("Green", t2.getColor());
-		assertEquals("Orange", t3.getColor());
+		assertEquals(new String[] {"R", "13"}, t.getInfo());
+		assertEquals(new String[] {"B", "1"}, t1.getInfo());
+		assertEquals(new String[] {"G", "7"}, t2.getInfo());
+		assertEquals(new String[] {"O", "9"}, t3.getInfo());
 	}
 }
 
