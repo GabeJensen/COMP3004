1cfa1fa9d00b7c8be9ab0d58a88356ef9891038c
GabeJensen <gabriel.jensen11@hotmail.ca>
2018-10-07 20:23:42 -0400

    Test: Added testing class for Player -Contains test for Player's name

--- src/test/java/core/PlayerTesting.java
+++ src/test/java/core/PlayerTesting.java
@@ -1,5 +1,15 @@
 package core;
 
-public class PlayerTesting {
+import junit.framework.TestCase;
 
+public class PlayerTesting extends TestCase{
+	
+	public void testPlayerName() {
+		Player jim = new Player("Jim");
+		Player noName = new Player();
+		
+		assertEquals(jim.getName(), "Jim");
+		assertEquals(noName.getName(), "No Name");
+	}
+	
 }
