258bdd4b2796b60aa6badb92b3c6d2573bd8a323
Alex Gao <alexgao1@users.noreply.github.com>
2018-10-11 22:06:58 -0400

    CODE: Player implements Observer, override update, give it reference to its Table subject

--- src/main/java/core/Player.java
+++ src/main/java/core/Player.java
@@ -1,19 +1,33 @@
 package core;
 
-public class Player {
+import java.util.ArrayList;
+import java.util.List;
+
+import tableObserver.Observer;
+import tableObserver.Subject;
+
+public class Player implements Observer {
 	private Hand hand;
 	private String name;
 	private boolean initial30;
+	private Subject table;
 			
-	public Player() {
+	public Player(Subject t) {
 		this.hand = new Hand();
 		this.name = "No Name";
 		this.initial30 = false;
+		this.table = t;
+		t.registerObserver(this);
 	}
 	
-	public Player(String name) {
+	public Player(Subject t, String name) {
 		this.name = name;
 		this.initial30 = false;
+		this.table = t;
+	}
+	
+	public void update(List<ArrayList<Tile>> tableMelds) {
+		// Player will use this table in some way, either to play hand melds and/or table melds.
 	}
 	
 	public String getName() {
