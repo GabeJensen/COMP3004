0ac6501bfc85ce89d49ced5453ca4ec39589eea0
Alex Gao <alexgao1@users.noreply.github.com>
2018-10-11 22:04:27 -0400

    TESTS: modified test case since Player requires Table subject

--- src/test/java/core/PlayerTesting.java
+++ src/test/java/core/PlayerTesting.java
@@ -1,12 +1,14 @@
 package core;
 
 import junit.framework.TestCase;
+import tableObserver.Table;
 
 public class PlayerTesting extends TestCase{
 	
 	public void testPlayerName() {
-		Player jim = new Player("Jim");
-		Player noName = new Player();
+		Table t = new Table();
+		Player jim = new Player(t, "Jim");
+		Player noName = new Player(t);
 		
 		assertEquals(jim.getName(), "Jim");
 		assertEquals(noName.getName(), "No Name");
