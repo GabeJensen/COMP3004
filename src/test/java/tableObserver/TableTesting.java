ceacd38810770616d4e18b4c34ab1cf979ddc078
ja-tsu <jason.tseun.hum@gmail.com>
2018-10-11 20:34:27 -0400

    test: added tests for adding a meld to Table

---
+++ src/test/java/tableObserver/TableTesting.java
@@ -0,0 +1,75 @@
+package tableObserver;
+
+import java.util.ArrayList;
+
+import core.Tile;
+import junit.framework.TestCase;
+import tableObserver.Table;
+
+public class TableTesting extends TestCase{
+	public void testAddingValidMelds() {
+		Table tb = new Table();
+		ArrayList<Tile> meld1 = new ArrayList<Tile>();
+		ArrayList<Tile> meld2 = new ArrayList<Tile>();
+		ArrayList<Tile> meld3 = new ArrayList<Tile>();
+		ArrayList<Tile> meld4 = new ArrayList<Tile>();
+		
+		meld1.add(new Tile("R", "6"));
+		meld1.add(new Tile("O", "6"));
+		meld1.add(new Tile("G", "6"));
+		
+		meld2.add(new Tile("B", "8"));
+		meld2.add(new Tile("B", "9"));
+		meld2.add(new Tile("B", "10"));
+		
+		meld3.add(new Tile("R", "12"));
+		meld3.add(new Tile("O", "12"));
+		meld3.add(new Tile("G", "12"));
+		meld3.add(new Tile("B", "12"));
+		
+		meld4.add(new Tile("O", "1"));
+		meld4.add(new Tile("O", "2"));
+		meld4.add(new Tile("O", "3"));
+		meld4.add(new Tile("O", "4"));
+		
+		assertEquals(true, tb.addMeldToTable(meld1));
+		assertEquals(true, tb.addMeldToTable(meld2));
+		assertEquals(true, tb.addMeldToTable(meld3));
+		assertEquals(true, tb.addMeldToTable(meld4));
+	}
+	
+	public void testAddingInvalidMelds() {
+		Table tb = new Table();
+		ArrayList<Tile> meld1 = new ArrayList<Tile>();
+		ArrayList<Tile> meld2 = new ArrayList<Tile>();
+		ArrayList<Tile> meld3 = new ArrayList<Tile>();
+		ArrayList<Tile> meld4 = new ArrayList<Tile>();
+		ArrayList<Tile> meld5 = new ArrayList<Tile>();
+		
+		meld1.add(new Tile("R", "6"));
+		meld1.add(new Tile("O", "6"));
+		
+		meld2.add(new Tile("B", "8"));
+		meld2.add(new Tile("B", "9"));
+		meld2.add(new Tile("B", "11"));
+		
+		meld3.add(new Tile("R", "12"));
+		meld3.add(new Tile("O", "12"));
+		meld3.add(new Tile("G", "12"));
+		meld3.add(new Tile("O", "12"));
+		
+		meld4.add(new Tile("O", "1"));
+		meld4.add(new Tile("O", "2"));
+		meld4.add(new Tile("O", "3"));
+		meld4.add(new Tile("G", "4"));
+		
+		meld5.add(new Tile("G", "6"));
+		meld5.add(new Tile("O", "7"));
+		
+		assertEquals(false, tb.addMeldToTable(meld1));
+		assertEquals(false, tb.addMeldToTable(meld2));
+		assertEquals(false, tb.addMeldToTable(meld3));
+		assertEquals(false, tb.addMeldToTable(meld4));
+		assertEquals(false, tb.addMeldToTable(meld5));
+	}
+}
