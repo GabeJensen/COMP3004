ef6be2e0c673eda1430e28a6797e2b58237ee7ce
GabeJensen <gabriel.jensen11@hotmail.ca>
2018-10-14 13:51:54 -0400

    Test: Checking addTile and getTiles function for Player class

--- src/test/java/core/PlayerTesting.java
+++ src/test/java/core/PlayerTesting.java
@@ -1,5 +1,9 @@
 package core;
 
+import static org.junit.Assert.assertArrayEquals;
+
+import java.util.ArrayList;
+
 import junit.framework.TestCase;
 import tableObserver.Table;
 
@@ -12,6 +16,30 @@ public class PlayerTesting extends TestCase{
 		
 		assertEquals(jim.getName(), "Jim");
 		assertEquals(noName.getName(), "No Name");
+	}
+	
+	public void testAddTiles() {
+		Table t = new Table();
+		Player player = new Player(t, new Strat0());
+		Tile tile1 = new Tile("R", "5");
+		Tile tile2 = new Tile("B", "5");
+		Tile tile3 = new Tile("B", "5");
+		
+		ArrayList<Tile> tiles = new ArrayList<Tile>();
+		
+		tiles.add(tile1);
+		tiles.add(tile2);
+		tiles.add(tile3);
+		
+		player.addTile(tile1);
+		player.addTile(tile2);
+		player.addTile(tile3);
+		
+		ArrayList<Tile> playerTiles = player.getTiles();
+		
+		for(int i = 0; i < tiles.size(); i++) {
+			assertArrayEquals(tiles.get(i).getInfo(), playerTiles.get(i).getInfo());
+		}
 	}
 	
 }
