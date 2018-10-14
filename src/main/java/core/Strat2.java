8a651a93fc34fa357d7241229ba5ae33e64b397b
Alex Gao <alexgao1@users.noreply.github.com>
2018-10-14 10:27:16 -0400

    CODE: modified function parameters to match the PlayerStrategy function

--- src/main/java/core/Strat2.java
+++ src/main/java/core/Strat2.java
@@ -1,7 +1,10 @@
 package core;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Strat2 implements PlayerStrategy{
-	public void strat() {
+	public void strat(Hand h, boolean initialMeld, List<ArrayList<Tile>> tableTiles) {
 		// Strategy 2
 		// Plays table melds if it won't win. If it will win in the current turn, plays hand melds.
 	}
--- src/main/java/core/Strat3.java
+++ src/main/java/core/Strat3.java
@@ -1,7 +1,10 @@
 package core;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Strat3 implements PlayerStrategy{
-	public void strat() {
+	public void strat(Hand h, boolean initialMeld, List<ArrayList<Tile>> tableTiles) {
 		// Strategy 3
 		// If the player has 3 or more cards than any other player, then it plays everything it can.
 		// Otherwise, if it does not, then it just plays table melds.
