954bdbfe11688af8cf26214f531bd29538e47fb5
Alex Gao <alexgao1@users.noreply.github.com>
2018-10-14 22:06:00 -0400

    CODE: GUI initial console elements, start button added

--- src/main/java/GUI/MainScreen.java
+++ src/main/java/GUI/MainScreen.java
@@ -2,10 +2,16 @@ package GUI;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class MainScreen extends Application {
+	
+	private Pane canvas;
+	private Scene scene;
 	
 	public static void main(String[] args) {
 		launch(args);
@@ -18,14 +24,54 @@ public class MainScreen extends Application {
 
 	private void initWindow(Stage primaryStage) {
 		Pane canvas = new Pane();
-		canvas.setStyle("-fx-background-color: #660f91"); //brown-ish colour
+		this.canvas = canvas;
+		canvas.setId("pane");
 		
-		//add labels/buttons/images
+		initCanvasElements(canvas);
 		
-		Scene scene = new Scene(canvas, 1280, 720);
+		Scene scene = new Scene(canvas, 1450, 900);
+		scene.getStylesheets().addAll(this.getClass().getResource("./style.css").toExternalForm());
 		primaryStage.setScene(scene);
+		primaryStage.setResizable(false);
 		primaryStage.setTitle("Team 8 - Tile Rummy");
+
+		this.scene = scene;
+		
 		primaryStage.show();
 	}
+	
+	private void displayToConsole(String s) {
+		TextArea console = (TextArea) scene.lookup("#console");
+		console.appendText(s + "\n");
+	}
 
+	private void initCanvasElements(Pane canvas) {
+		// Console (side text are)
+		TextArea console = new TextArea();
+		console.setPrefHeight(900);
+		console.setPrefWidth(300);
+		console.relocate(1155,5);
+		console.setEditable(false);
+		console.setMouseTransparent(true);
+		console.setFocusTraversable(false);
+		console.setWrapText(true);
+		console.setId("console");
+		
+		console.textProperty().addListener((element, oldValue, newValue) -> {
+			// Auto scroll to bottom when appending text to console pane
+			console.setScrollTop(Double.MAX_VALUE);
+		});
+		
+		// Start Button
+		Button start = new Button("Start Game");
+		start.relocate(1155/2, 900/2);
+		
+		start.addEventHandler(MouseEvent.MOUSE_CLICKED, ev -> {
+			displayToConsole("Started a game of Tile Rummy!");
+			canvas.getChildren().remove(start);
+		});
+		
+		// Add all elements created to primary canvas node here.
+		canvas.getChildren().addAll(console, start);
+	}
 }
