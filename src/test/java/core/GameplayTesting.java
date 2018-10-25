ea5ca4ed18a79f0266670b3db2f01e7be20bc5a7
ja-tsu <jason.tseun.hum@gmail.com>
2018-10-24 22:40:43 -0400

    test: start of testStrat2 (not done yet)

--- src/test/java/core/GameplayTesting.java
+++ src/test/java/core/GameplayTesting.java
@@ -74,4 +74,62 @@ public class GameplayTesting extends TestCase{
 			assertArrayEquals(newMeld1[i].getInfo(), tableGetter.get(0).get(i).getInfo());
 		}
 	}
+	
+	public void testStrat2() {
+		int stratResult;
+		Table table = new Table();
+		Player p2 = new Player(table, "P2", new Strat2());
+
+		Tile tile1 = new Tile("R", "1");
+		Tile tile2 = new Tile("R", "2");
+		Tile tile3 = new Tile("R", "3");
+		Tile tile4 = new Tile("R", "10");
+		Tile tile5 = new Tile("B", "10");
+		Tile tile6 = new Tile("G", "10");
+		Tile tile7 = new Tile("O", "10");
+		Tile tile8 = new Tile("B", "5");
+		Tile tile9 = new Tile("O", "7");
+		Tile tile10 = new Tile("R", "5");
+		Tile tile11 = new Tile("B", "5");
+		Tile tile12 = new Tile("B", "6");
+		Tile tile13 = new Tile("O", "8");
+		
+		Tile[] handTiles = {tile1, tile2, tile3, tile4, tile5, tile6, tile7, tile8, tile9, tile10, tile11, tile12, tile13};
+		Tile[] tableMeld = {new Tile("R", "11"), new Tile("R", "12"), new Tile("R", "13")};
+		Tile[] meld1 = {tile1, tile2, tile3};
+		Tile[] initialMeldTiles = {tile4, tile5, tile6, tile7};
+		
+		ArrayList<Tile> firstTableMeld = new ArrayList<Tile>();
+		ArrayList<Tile> meldOne = new ArrayList<Tile>();
+		ArrayList<Tile> initialMeldToPlay = new ArrayList<Tile>();
+		
+		for (Tile t: handTiles) {
+			p2.addTile(t);
+		}
+		for (Tile t: tableMeld) {
+			firstTableMeld.add(t);
+		}
+		for (Tile t: meld1) {
+			meldOne.add(t);
+		}
+		for (Tile t: initialMeldTiles) {
+			initialMeldToPlay.add(t);
+		}
+		
+		//table is empty, hand has meld > 30, draw card
+		stratResult = p2.performStrategy();
+		assertEquals(0, stratResult);		
+		
+		//another player has played their initial meld > 30
+		table.addMeldToTable(firstTableMeld);
+		
+		//table is not empty, hand has meld > 30, play that meld
+		stratResult = p2.performStrategy();
+		assertEquals(1, stratResult);
+		
+		List<ArrayList<Tile>> meldsOnTable = table.getTable();
+		
+		//check if performStrategy() play the correct initial meld
+		assertArrayEquals(initialMeldToPlay.toArray(), meldsOnTable.get(1).toArray());
+	}
 }
