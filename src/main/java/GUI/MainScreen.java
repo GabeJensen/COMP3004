2fa649159984cb79a69f4248650553ceba663df9
ja-tsu <jason.tseun.hum@gmail.com>
2018-10-30 21:14:45 -0400

    refactor: tidy up code by extracting code and putting them into their own method 		- renamed attributes

--- src/main/java/GUI/MainScreen.java
+++ src/main/java/GUI/MainScreen.java
@@ -42,15 +42,15 @@ public class MainScreen extends Application {
 	private Scene scene;
 	private HashMap<String, String> imageLoc;
 	private HashMap<ImageView, Tile> associatedTiles;
-	private TextArea console;
-	private Button start;
-	private Button end;
-	private Button undo;
-	//private Button sort;
-	private Button playMeld;
-	private VBox meldArea;
-	private HBox userTiles;
-	private HBox topCommands;
+	private TextArea consoleTextArea;
+	private Button startButton;
+	private Button endButton;
+	private Button undoButton;
+	//private Button sortButton;
+	private Button playMeldButton;
+	private VBox playMeldBox;
+	private HBox userTilesBox;
+	private HBox topCommandsBox;
 	private TilePane playGrid;
 	
 	private ArrayList<ArrayList<Tile>> currentTurnMelds;
@@ -70,29 +70,6 @@ public class MainScreen extends Application {
 		initWindow(primaryStage);
 	}
 
-	private void initWindow(Stage primaryStage) {
-		BorderPane canvas = new BorderPane();
-		this.canvas = canvas;
-		canvas.setId("canvas");
-		
-		initCanvasElements();
-		
-		Scene scene = new Scene(canvas, 1450, 900);
-		scene.getStylesheets().addAll(this.getClass().getResource("./style.css").toExternalForm());
-		primaryStage.setScene(scene);
-		primaryStage.setResizable(false);
-		primaryStage.setTitle("Team 8 - Tile Rummy");
-
-		this.scene = scene;
-		
-		primaryStage.show();
-	}
-	
-	private void displayToConsole(String s) {
-		TextArea console = (TextArea) scene.lookup("#console");
-		console.appendText(s + "\n");
-	}
-	
 	public void playGame() {
 		//Create and initialize GUI
 		/* Create buttons for:
@@ -140,7 +117,7 @@ public class MainScreen extends Application {
 		table.addMeldToTable(m);
 		//table.addMeldToTable(m1);
 		//table.addMeldToTable(m2);
-
+	
 		// move these 3 lines into the game loop
 		associatedTiles.clear();		
 		updateDisplayHand();
@@ -156,6 +133,163 @@ public class MainScreen extends Application {
 		 * 		GUI.update()
 		 * 	*/
 	}
+
+	private void initWindow(Stage primaryStage) {
+		BorderPane canvas = new BorderPane();
+		this.canvas = canvas;
+		canvas.setId("canvas");
+		
+		initCanvasElements();
+		
+		Scene scene = new Scene(canvas, 1450, 900);
+		scene.getStylesheets().addAll(this.getClass().getResource("./style.css").toExternalForm());
+		primaryStage.setScene(scene);
+		primaryStage.setResizable(false);
+		primaryStage.setTitle("Team 8 - Tile Rummy");
+
+		this.scene = scene;
+		
+		primaryStage.show();
+	}
+	
+	private void initCanvasElements() {
+		// Console (side text are)
+		initConsole();
+		
+		// Preload the tile info
+		loadTileImages();
+		
+		// Start Button
+		startButton = new Button("Start Game");
+		//startButton.relocate(1155/2, 900/2);
+		
+		startButton.addEventHandler(MouseEvent.MOUSE_CLICKED, ev -> {
+			displayToConsole("Started a game of Tile Rummy!");
+			canvas.setCenter(null);
+			initGameElements();
+		});
+		
+		// Set elements on the BorderPane areas
+		canvas.setRight(consoleTextArea);
+		canvas.setCenter(startButton);
+	}
+
+	private void initGameElements() {
+		// Bottom card section
+		userTilesBox = new HBox();
+		userTilesBox.setPadding(new Insets(5,5,0,5));
+		userTilesBox.setSpacing(10);
+		
+		// Commands at top
+		initTopCommands();
+		
+		// Left borderPane column for melds
+		initMeldArea();
+		
+		// Grid in middle
+		playGrid = new TilePane();
+		playGrid.setPadding(new Insets(10, 2, 10, 2));
+		playGrid.setVgap(5);
+		playGrid.setHgap(2);
+		playGrid.setPrefRows(8);
+
+		/*sort.addEventHandler(MouseEvent.MOUSE_CLICKED, ev -> {
+			ObservableList<Node> uT = FXCollections.observableArrayList();
+			uT = userTilesBox.getChildren();
+			System.out.println(uT);
+		});*/
+		
+		playMeldBox.setPrefWidth(350);
+		
+		canvas.setCenter(playGrid);
+		canvas.setTop(topCommandsBox);
+		canvas.setBottom(userTilesBox);
+		canvas.setLeft(playMeldBox);
+		//userTilesBox.getChildren().addAll();
+		
+		playGame();
+	}
+
+	private void initConsole() {
+		consoleTextArea = new TextArea();
+		consoleTextArea.setPrefHeight(900);
+		consoleTextArea.setPrefWidth(300);
+		//console.relocate(1155,5);
+		consoleTextArea.setEditable(false);
+		consoleTextArea.setMouseTransparent(true);
+		consoleTextArea.setFocusTraversable(false);
+		consoleTextArea.setWrapText(true);
+		consoleTextArea.setId("console");
+		
+		consoleTextArea.textProperty().addListener((element, oldValue, newValue) -> {
+			// Auto scroll to bottom when appending text to console pane
+			consoleTextArea.setScrollTop(Double.MAX_VALUE);
+		});
+	}
+
+	private void loadTileImages() {
+		String tileResources = "src/main/resources/";
+		File tileLoc = new File(tileResources);
+		File[] tileImages = tileLoc.listFiles();
+		imageLoc = new HashMap<String, String>();
+		associatedTiles = new HashMap<ImageView, Tile>();
+		for (int i = 0; i < tileImages.length; i++) {
+			String fileName = tileImages[i].getName();
+			String[] imgInf = fileName.split(Pattern.quote("."));
+			imageLoc.put(imgInf[0], tileResources + tileImages[i].getName());
+		}
+	}
+
+	private void initMeldArea() {
+		playMeldBox = new VBox();
+		playMeldBox.setPadding(new Insets(20,0,0,25));
+		playMeldButton = new Button("   Play Meld   ");
+		playMeldButton.addEventHandler(MouseEvent.MOUSE_CLICKED, ev -> {
+			ArrayList<Tile> meldTiles = new ArrayList<Tile>();
+			// Start from 1, since that's always the Play Meld button.
+			for (int c = 1; c < playMeldBox.getChildren().size(); c++) {
+				meldTiles.add(associatedTiles.get(playMeldBox.getChildren().get(c)));
+			}
+			if (Meld.checkValidity(meldTiles)) {
+				currentTurnMelds.add(meldTiles);
+				meldTiles.clear();
+			}
+			else {
+				// Invalid meld
+				displayToConsole("That meld is not valid.");
+			}
+			
+		});
+		
+		playMeldBox.getChildren().addAll(playMeldButton);
+	}
+
+	private void initTopCommands() {
+		topCommandsBox = new HBox();
+		topCommandsBox.setPadding(new Insets(5,10,0,10));
+		topCommandsBox.setSpacing(10);
+		
+		endButton = new Button("End Turn");
+		endButton.addEventHandler(MouseEvent.MOUSE_CLICKED, ev -> {
+			displayToConsole("DEBUG: to end turn");
+		});
+		
+		undoButton = new Button("Undo Turn");
+		undoButton.addEventHandler(MouseEvent.MOUSE_CLICKED, ev -> {
+			displayToConsole("This feature is unlockable via DLC.");
+		});
+		
+		//sortButton = new Button ("Sort Hand");
+		// Memento pattern. 
+		undoButton.setDisable(true);
+		// Remove the above button when undo is actually implemented.
+		topCommandsBox.getChildren().addAll(endButton, undoButton);
+	}
+
+	private void displayToConsole(String s) {
+		TextArea console = (TextArea) scene.lookup("#console");
+		console.appendText(s + "\n");
+	}
 	
 	private void updateDisplayTable() {
 		clearDisplayTable();
@@ -169,7 +303,7 @@ public class MainScreen extends Application {
 						dTile.isOrigin = !dTile.isOrigin;
 						dTile.lastIndex = playGrid.getChildren().indexOf(ev.getSource());
 						playGrid.getChildren().set(dTile.lastIndex, new Region());
-						meldArea.getChildren().add(dTile.iv);
+						playMeldBox.getChildren().add(dTile.iv);
 					}
 					else {
 						playGrid.getChildren().set(dTile.lastIndex, dTile.iv);
@@ -197,142 +331,25 @@ public class MainScreen extends Application {
 			dTile.iv.addEventHandler(MouseEvent.MOUSE_CLICKED, ev -> {
 				if (dTile.isOrigin) {
 					// In original spot
-					dTile.lastIndex = userTiles.getChildren().indexOf(ev.getSource());
+					dTile.lastIndex = userTilesBox.getChildren().indexOf(ev.getSource());
 					//dTile.setOriginCoordinates(ev.getSceneX(), ev.getSceneY());
 					dTile.isOrigin = !dTile.isOrigin;
-					userTiles.getChildren().set(dTile.lastIndex, new Region());
-					meldArea.getChildren().add(dTile.iv);
+					userTilesBox.getChildren().set(dTile.lastIndex, new Region());
+					playMeldBox.getChildren().add(dTile.iv);
 				}
 				else {
 					// Not in original spot, move it back there
-					//meldArea.getChildren().remove(dTile.iv);
-					userTiles.getChildren().set(dTile.lastIndex, dTile.iv);
+					//playMeldBox.getChildren().remove(dTile.iv);
+					userTilesBox.getChildren().set(dTile.lastIndex, dTile.iv);
 					dTile.isOrigin = !dTile.isOrigin;
 				}
 			});
-			userTiles.getChildren().addAll(dTile.iv);
+			userTilesBox.getChildren().addAll(dTile.iv);
 			associatedTiles.put(dTile.iv, userTile);
 		}
 	}
 	
 	private void clearDisplayHand() {
-		userTiles.getChildren().clear();
-	}
-	
-	private void initGameElements() {
-		// Bottom card section
-		userTiles = new HBox();
-		userTiles.setPadding(new Insets(5,5,0,5));
-		userTiles.setSpacing(10);
-		
-		// Commands at top
-		topCommands = new HBox();
-		topCommands.setPadding(new Insets(5,10,0,10));
-		topCommands.setSpacing(10);
-		end = new Button("End Turn");
-		undo = new Button("Undo Turn");
-		//sort = new Button ("Sort Hand");
-		// Memento pattern. 
-		undo.setDisable(true);
-		// Remove the above button when undo is actually implemented.
-		topCommands.getChildren().addAll(end, undo);
-		
-		// Left borderPane column for melds
-		meldArea = new VBox();
-		meldArea.setPadding(new Insets(20,0,0,25));
-		playMeld = new Button("   Play Meld   ");
-		playMeld.addEventHandler(MouseEvent.MOUSE_CLICKED, ev -> {
-			ArrayList<Tile> meldTiles = new ArrayList<Tile>();
-			// Start from 1, since that's always the Play Meld button.
-			for (int c = 1; c < meldArea.getChildren().size(); c++) {
-				meldTiles.add(associatedTiles.get(meldArea.getChildren().get(c)));
-			}
-			if (Meld.checkValidity(meldTiles)) {
-				currentTurnMelds.add(meldTiles);
-				meldTiles.clear();
-			}
-			else {
-				// Invalid meld
-				displayToConsole("That meld is not valid.");
-			}
-			
-		});
-		
-		meldArea.getChildren().addAll(playMeld);
-		
-		// Grid in middle
-		playGrid = new TilePane();
-		playGrid.setPadding(new Insets(10, 2, 10, 2));
-		playGrid.setVgap(5);
-		playGrid.setHgap(2);
-		playGrid.setPrefRows(8);
-		
-		end.addEventHandler(MouseEvent.MOUSE_CLICKED, ev -> {
-			displayToConsole("DEBUG: to end turn");
-		});
-		
-		undo.addEventHandler(MouseEvent.MOUSE_CLICKED, ev -> {
-			displayToConsole("This feature is unlockable via DLC.");
-		});
-		
-		/*sort.addEventHandler(MouseEvent.MOUSE_CLICKED, ev -> {
-			ObservableList<Node> uT = FXCollections.observableArrayList();
-			uT = userTiles.getChildren();
-			System.out.println(uT);
-		});*/
-		
-		meldArea.setPrefWidth(350);
-		
-		canvas.setCenter(playGrid);
-		canvas.setTop(topCommands);
-		canvas.setBottom(userTiles);
-		canvas.setLeft(meldArea);
-		//userTiles.getChildren().addAll();
-		
-		playGame();
-	}
-
-	private void initCanvasElements() {
-		// Console (side text are)
-		console = new TextArea();
-		console.setPrefHeight(900);
-		console.setPrefWidth(300);
-		//console.relocate(1155,5);
-		console.setEditable(false);
-		console.setMouseTransparent(true);
-		console.setFocusTraversable(false);
-		console.setWrapText(true);
-		console.setId("console");
-		
-		console.textProperty().addListener((element, oldValue, newValue) -> {
-			// Auto scroll to bottom when appending text to console pane
-			console.setScrollTop(Double.MAX_VALUE);
-		});
-		
-		// Preload the tile info
-		String tileResources = "src/main/resources/";
-		File tileLoc = new File(tileResources);
-		File[] tileImages = tileLoc.listFiles();
-		imageLoc = new HashMap<String, String>();
-		associatedTiles = new HashMap<ImageView, Tile>();
-		for (int i = 0; i < tileImages.length; i++) {
-			String fileName = tileImages[i].getName();
-			String[] imgInf = fileName.split(Pattern.quote("."));
-			imageLoc.put(imgInf[0], tileResources + tileImages[i].getName());
-		}
-		
-		// Start Button
-		start = new Button("Start Game");
-		//start.relocate(1155/2, 900/2);
-		
-		start.addEventHandler(MouseEvent.MOUSE_CLICKED, ev -> {
-			displayToConsole("Started a game of Tile Rummy!");
-			canvas.setCenter(null);
-			initGameElements();
-		});
-		
-		// Set elements on the BorderPane areas
-		canvas.setRight(console);
-		canvas.setCenter(start);
+		userTilesBox.getChildren().clear();
 	}
 }
