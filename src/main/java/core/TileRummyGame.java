90a4121c2eff72914df2d2c0f868835d95f2a7a0
GabeJensen <gabriel.jensen11@hotmail.ca>
2018-11-15 20:42:30 -0500

    Code: Added functionality for variable number of players/users - Multiple users can play - Players are ordered in a circular queue for loop purposes - Game now loops through players in the queue and plays their turn   depending on if they're human or AI - Game cannot handle an all AI game yet

--- src/main/java/core/TileRummyGame.java
+++ src/main/java/core/TileRummyGame.java
@@ -1,7 +1,9 @@
 package core;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 import GUI.GUI;
 import observer.Game;
@@ -13,6 +15,8 @@ public class TileRummyGame {
 	private Player p2;
 	private Player p3;
 	private ArrayList<Player> players;
+	private Queue<Player> orderedPlayers;
+	private Player currentPlayer;
 	private Deck deck;
 	private Originator originator;
 	private Caretaker caretaker;
@@ -22,6 +26,7 @@ public class TileRummyGame {
 		javafx.application.Application.launch(GUI.class);
 	}
 	
+	@Deprecated
 	public void initalizeGame() {
 		game = new Game();
 		originator = new Originator();
@@ -43,9 +48,6 @@ public class TileRummyGame {
 			players.add(new Player(game, "User " + (i+1), new Strat0()));
 		}
 		
-		/*int p1Count = 0;
-		int p2Count = 0;
-		int p3Count = 0;*/
 		//Goes throgh al strategies and names them appropriately
 		for(int i = 0; i < strategySelection.size(); i++) {
 			String selection = strategySelection.get(i);
@@ -60,12 +62,17 @@ public class TileRummyGame {
 				// Strategy 4...
 			}
 		}
-		//TEMP REMOVE AFTER FIX
-		user = players.get(0);
-		this.p1 = players.get(1);
-		this.p2 = players.get(2);
-		this.p3 = players.get(3);
+
 		game.setPlayers(players);
+	}
+	
+	public void displayOtherUserHands() {
+		for(Player player : players) {
+			if(currentPlayer != player) {
+				GUI.displayToConsole(player.getName() + "'s hand: " + player.getHand());
+			}
+		}
+		GUI.displayToConsole("----------");
 	}
 	
 	public void playGame() {
@@ -80,19 +87,88 @@ public class TileRummyGame {
 			}
 		}
 		
-		GUI.updateDisplayHand(user.getTiles());
-		GUI.updateDisplayTable(game.getTable());
+		//decideOrder() function goes here
+		//For now i will manually put the order
+		orderedPlayers = new LinkedList<Player>();
+		for(Player player : players) {
+			orderedPlayers.add(player);
+		}
 		
-		// Because apparently we need to display everyone's hand at the start as well.
-		GUI.displayToConsole("P1's hand: " + p1.getHand());
-		GUI.displayToConsole("P2's hand: " + p2.getHand());
-		GUI.displayToConsole("P3's hand: " + p3.getHand());
-		
-		// Start of the game. We still want to be able to revert here.
-		originator.setState(game.getTable(), user.getTiles());
-		caretaker.add(user.getName(), originator.saveMemento());
+		nextTurn();
 	}
 	
+	public void next() {
+		//Assigns current player to next player in line and puts them at the back creating a circular queue
+		currentPlayer = orderedPlayers.remove();
+		orderedPlayers.add(currentPlayer);
+	}
+	
+	public void nextTurn() {
+		int turnValue;
+		Tile tile;
+		
+		next();
+		/*New Idea
+		 * Instead of loop we have a nextTurn function
+		 * Goes to next player in line and performs their action
+		 * if human do human stuff else do ai stuff
+		 * AI stuff we have implemented more or less
+		 * On human side we just update the playArea and have whatever user play their hand and end turn
+		 * NOTE: I am not sure how the GUI handles what player is playing and if the tiles in the play area would 
+		 * properly reflect the player who's tiles those belong to
+		 * */
+		
+		if(currentPlayer.getName().contains("User")) {
+			//User stuff
+			GUI.updateDisplayHand(currentPlayer.getTiles());
+			
+			originator.setState(game.getTable(), currentPlayer.getTiles());
+			caretaker.add(currentPlayer.getName(), originator.saveMemento());
+			GUI.displayToConsole(currentPlayer.getName() + "'s turn	");
+			displayOtherUserHands();
+			
+			//Needs to wait for end turn input
+		} else {
+			//Ai stuff
+			aiTurn();
+			nextTurn();
+		}
+		
+	}
+	
+	public void aiTurn() {
+		int turnValue;
+		Tile tile;
+		turnValue = currentPlayer.performStrategy();
+		if (turnValue == 0) {
+			if(!emptyDeck) {
+				tile = deck.dealTile();
+				if(tile == null) {
+					emptyDeck = true;
+					GUI.displayToConsole(currentPlayer.getName() + " tried drawing, but the deck was empty!");
+				} else {
+					currentPlayer.addTile(tile);
+					GUI.displayToConsole(currentPlayer.getName() + " draws " + tile.toString() + "!" );
+				}
+			} else {
+				GUI.displayToConsole(currentPlayer.getName() + " can't draw because the deck is empty!");
+			}
+			GUI.displayToConsole(currentPlayer.getName() + "'s hand: " + currentPlayer.getHand());
+		}
+		else if (turnValue == 1) {
+			GUI.displayToConsole(currentPlayer.getName() + " played this turn!");
+			if(currentPlayer.getHandCount() == 0) {
+				GUI.displayToConsole(currentPlayer.getName() + " says: 'RUMMIKUB!' They won the game!");
+				GUI.disableButtons();
+			} else {
+				GUI.displayToConsole(currentPlayer.getName() + "'s hand: " + currentPlayer.getHand());				
+			}
+		}
+		
+		GUI.updateDisplayTable(game.getTable());
+	}
+	
+	@Deprecated
 	public void gameLoop() {
 		Player[] nonHumanPlayers = {p1, p2, p3};
 		int turnValue;
@@ -127,20 +203,19 @@ public class TileRummyGame {
 			}
 		}		
 		
-		GUI.updateDisplayHand(user.getTiles());
+		GUI.updateDisplayHand(currentPlayer.getTiles());
 		GUI.updateDisplayTable(game.getTable());
-		originator.setState(game.getTable(), user.getTiles());
-		caretaker.add(user.getName(), originator.saveMemento());
+		originator.setState(game.getTable(), currentPlayer.getTiles());
+		caretaker.add(currentPlayer.getName(), originator.saveMemento());
 	}
 	
 	public void undoTurn() {
-		//TODO: We will need to consider setting a "current player" per turn.
-		originator.restoreMemento(caretaker.get(user.getName()));
+		originator.restoreMemento(caretaker.get(currentPlayer.getName()));
 		
 		game.setTable(originator.getState().getTable());
-		user.setTiles(originator.getState().getHand());
+		currentPlayer.setTiles(originator.getState().getHand());
 		
-		GUI.updateDisplayHand(user.getTiles());
+		GUI.updateDisplayHand(currentPlayer.getTiles());
 		GUI.updateDisplayTable(game.getTable());
 	}
 	
@@ -157,17 +232,17 @@ public class TileRummyGame {
 				Tile draw = deck.dealTile();
 				if(draw == null) {
 					emptyDeck = true;
-					GUI.displayToConsole(user.getName() + " tried drawing, but the deck was empty!");
+					GUI.displayToConsole(currentPlayer.getName() + " tried drawing, but the deck was empty!");
 				} else {
-					GUI.displayToConsole("User draws " + draw.toString() + "!");
-					user.addTile(draw);						
+					GUI.displayToConsole(currentPlayer.getName() + " draws " + draw.toString() + "!");
+					currentPlayer.addTile(draw);						
 				}
 			} else {
-				GUI.displayToConsole(user.getName() + " tried drawing, but the deck was empty!");
+				GUI.displayToConsole(currentPlayer.getName() + " tried drawing, but the deck was empty!");
 			}
 		} else {
 			// If the user has not played their initial 30 meld yet
-			if (!user.getInit30Flag()){
+			if (!currentPlayer.getInit30Flag()){
 				// No table tiles allowed to be played until the initial 30 meld(s)
 				int tileCount = 0;
 				for (ArrayList<Tile> meld : currentTurnMelds) {
@@ -186,7 +261,7 @@ public class TileRummyGame {
 						GUI.displayToConsole("The value of the hand melds played do not add up to at least 30 points!");
 						return;
 					} else {
-						user.playedInit30();
+						currentPlayer.playedInit30();
 					}
 				} else {
 					GUI.displayToConsole("You must play the the initial greater than 30 valued hand meld(s) first!");
@@ -206,16 +281,16 @@ public class TileRummyGame {
 		// At this point, all rule checking is complete
 		// Remove the user used tiles from user hand
 		for (int i = 0; i < currentTurnUserUsedTiles.size(); ++i) {
-			user.removeTile(currentTurnUserUsedTiles.get(i));
+			currentPlayer.removeTile(currentTurnUserUsedTiles.get(i));
 		}
 		game.setTable(turnTableState);
 		
 		// Check if human player on this turn has won or nots
-		if(user.getHandCount() == 0) {
-			GUI.displayToConsole(user.getName() + " says: 'RUMMIKUB!' They won the game!");
+		if(currentPlayer.getHandCount() == 0) {
+			GUI.displayToConsole(currentPlayer.getName() + " says: 'RUMMIKUB!' They won the game!");
 			GUI.disableButtons();
 		} else {
-			gameLoop();
+			nextTurn();
 		}
 	}
 }
