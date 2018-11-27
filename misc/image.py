e43015be944e9d488e405c8086bb8ea5f1ae8eab
GabeJensen <gabriel.jensen11@hotmail.ca>
2018-11-27 18:43:22 -0500

    Python code: Added png file for Joker tile

--- misc/image.py
+++ misc/image.py
@@ -5,17 +5,17 @@ source_img = Image.open("rec.png").convert("RGBA")
 card_color = "green"
 color = "G"
 
-for x in range(1, 14):
-    draw = ImageDraw.Draw(source_img)
-    draw.rectangle(((0, 00), (125, 153)), fill="white")
-    if (x > 9):
-        draw.text((33-27, 57-16), color + str(x), fill=card_color, font=ImageFont.truetype("arial.ttf", 60))
-    else:
-        draw.text((43-24, 57-16), color + str(x), fill=card_color, font=ImageFont.truetype("arial.ttf", 60))
-
-    source_img.save(color + str(x) + ".png", "PNG")
+# for x in range(1, 14):
+#     draw = ImageDraw.Draw(source_img)
+#     draw.rectangle(((0, 00), (125, 153)), fill="white")
+#     if (x > 9):
+#         draw.text((33-27, 57-16), color + str(x), fill=card_color, font=ImageFont.truetype("arial.ttf", 60))
+#     else:
+#         draw.text((43-24, 57-16), color + str(x), fill=card_color, font=ImageFont.truetype("arial.ttf", 60))
+#
+#     source_img.save(color + str(x) + ".png", "PNG")
 
 draw = ImageDraw.Draw(source_img)
 draw.rectangle(((0, 00), (125, 153)), fill="white")
-draw.text((43-24, 57-16), color + "J", fill=card_color, font=ImageFont.truetype("arial.ttf", 60))
-source_img.save(color + "J.png", "PNG")
+draw.text((43, 57-16), "J", fill="black", font=ImageFont.truetype("arial.ttf", 60))
+source_img.save("J.png", "PNG")
